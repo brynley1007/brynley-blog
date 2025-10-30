e757debc4f668bbb38d5725aeec5d308a7478644
brynley1007 <brynley1007@gmail.com>
2025-10-30 19:39:08 +0000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://grist-yates.uk/",
+  site: "https://grist-yates.uk",
   integrations: [mdx(), sitemap()],
   adapter: cloudflare({
     platformProxy: {
